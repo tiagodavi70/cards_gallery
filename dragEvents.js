26775508df8771c017caf726eadaa709fc194af4
Tiago Araújo <tiagodavi70@gmail.com>
2021-03-07 22:23:27 -0300

    fixed size, disabled resize

--- dragEvents.js
+++ dragEvents.js
@@ -2,47 +2,7 @@ import interact from 'https://cdn.interactjs.io/v1.10.8/interactjs/index.js'
 
 function setInteract(id) {
 // target elements with the "item" class
-    interact(id).resizable({
-        // resize from all edges and corners
-        edges: { left: true, right: true, bottom: true, top: true },
-
-        listeners: {
-            move (event) {
-                let target = event.target
-                let x = (parseFloat(target.getAttribute('data-x')) || 0)
-                let y = (parseFloat(target.getAttribute('data-y')) || 0)
-
-                // update the element's style
-                target.style.width = event.rect.width + 'px'
-                target.style.height = event.rect.height + 'px'
-
-                // translate when resizing from top or left edges
-                x += event.deltaRect.left
-                y += event.deltaRect.top
-
-                target.style.webkitTransform = target.style.transform =
-                'translate(' + x + 'px,' + y + 'px)'
-
-                target.setAttribute('data-x', x)
-                target.setAttribute('data-y', y)
-                // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
-            }
-        },
-        modifiers: [
-            // keep the edges inside the parent
-            interact.modifiers.restrictEdges({
-                outer: 'parent'
-            }),interact.modifiers.aspectRatio({
-                ratio: .7
-            }),
-            // minimum size
-            interact.modifiers.restrictSize({
-                min: { width: 70, height: 100 },
-                max: { width: 210, height: 300  }
-            })
-        ],
-        inertia: true
-  }).draggable({
+    interact(id).draggable({
     // enable inertial throwing
     inertia: true,
     // keep the element within the area of it's parent
@@ -65,7 +25,49 @@ function setInteract(id) {
       }
     }
   })
+  /*
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
 
+    listeners: {
+        move (event) {
+            let target = event.target
+            let x = (parseFloat(target.getAttribute('data-x')) || 0)
+            let y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+            // update the element's style
+            target.style.width = event.rect.width + 'px'
+            target.style.height = event.rect.height + 'px'
+
+            // translate when resizing from top or left edges
+            x += event.deltaRect.left
+            y += event.deltaRect.top
+
+            target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)'
+
+            target.setAttribute('data-x', x)
+            target.setAttribute('data-y', y)
+            // target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+        }
+    },
+    modifiers: [
+        // keep the edges inside the parent
+        interact.modifiers.restrictEdges({
+            outer: 'parent'
+        }),interact.modifiers.aspectRatio({
+            ratio: .7
+        }),
+        // minimum size
+        interact.modifiers.restrictSize({
+            min: { width: 70, height: 100 },
+            max: { width: 210, height: 300  }
+        })
+    ],
+    inertia: true
+  })
+  //*/
 }
 
 function dragMoveListener (event) {
